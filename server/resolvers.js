c77b6f365983db43dd5e97deeefe6b6deb7a90e0
Eric <Natelsoneric@gmail.com>
2020-06-21 16:09:07 -0500

    refactored for gh pages deploy

--- server/resolvers.js
+++
@@ -1,9 +0,0 @@
-
-const rootValue = {
-    hello: () => 'Hello, world',
-    name: () => 'Eric Natelson',
-    introduction: () => 'Hello! This is an interactive resume built with GraphQL. Start a query with something like query(education) ',
-    education: () => 'Florida State University'
-}
-
-module.exports = { rootValue };
